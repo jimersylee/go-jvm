81179c8a211d73fc20dfa1468133b95c3c0b45ed
Jimersy Lee <jimersylee@gmail.com>
2019-02-02 23:06:56 +0800

    add:ch02

---
+++ ch02/classpath/entry_dir.go
@@ -0,0 +1,25 @@
+package classpath
+
+import (
+	"io/ioutil"
+	"path/filepath"
+)
+
+type DirEntry struct {
+	absDir string
+}
+
+func newDirEntry(path string) *DirEntry {
+
+	return nil
+}
+
+func (self *DirEntry) readClass(className string) ([]byte, Entry, error) {
+	fileName := filepath.Join(self.absDir, className)
+	data, err := ioutil.ReadFile(fileName)
+	return data, self, err
+}
+
+func (self DirEntry) String() string {
+	return ""
+}
